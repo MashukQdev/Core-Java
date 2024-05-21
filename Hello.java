5bcb2e9f705e6972dab2a56b1c9718e1296ebd72
MashukQdev <mashukqdev@gmail.com>
2024-05-21 11:16:23 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,17 @@
+package com.main;
 
+/**
+Simple program to print hello world
+Practical 1 from core java practicals
+@author Mashuk Patel
+ */
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		
+	}
+	
+}
