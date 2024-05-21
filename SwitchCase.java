14e45620d5e89903f30ab30a6277893c0f69e8da
MashukQdev <mashukqdev@gmail.com>
2024-05-21 18:28:22 +0530

    SwitchCase.java

--- SwitchCase.java
+++ SwitchCase.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class SwitchCase {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number to find month name : ");
 		int monthnumber = sc.nextInt();
 		switch(monthnumber) {
