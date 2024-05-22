8f61dedb0ef6ad63e320cf0ac82e3dd065fbc40b
MashukQdev <mashukqdev@gmail.com>
2024-05-22 08:09:02 +0530

    BreakContinueStatement.java

--- BreakContinueStatement.java
+++ BreakContinueStatement.java
@@ -1,1 +1,24 @@
+package com.main;
 
+/**
+ Simple program to demonstrate use of break / continue statement using for loop
+ Practical 6 from core java practicals
+ @author Mashuk Patel
+ */
+public class BreakContinueStatement {
+
+	public static void main(String[] args) {
+		for(int i=1; i <= 10; i++) {
+			//skip number 5 by using continue statement
+			if(i == 5) {
+				continue;
+			}
+			//stop the loop before printing number 8 using break statement
+			if(i == 8) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+	
+}
