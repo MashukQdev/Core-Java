ebd726e3d0dc7242abe09d905d97d14293a033cb
MashukQdev <mashukqdev@gmail.com>
2024-05-22 16:37:19 +0530

    ThisKeyword.java

--- ThisKeyword.java
+++ ThisKeyword.java
@@ -1,1 +1,25 @@
+package com.main;
 
+/**
+ Simple program to demonstrate use of this keyword
+ Practical 11 from core java practicals
+ @author Mashuk Patel 
+ */
+public class ThisKeyword {
+
+	private String name;
+	private int id;
+	
+	public ThisKeyword(String name, int id) {
+		// we use this keyword when parameters and instance variable are same 
+		this.name=name;
+		this.id=id;
+	}
+	
+	public static void main(String[] args) {
+		ThisKeyword keyword = new ThisKeyword("Mashuk", 7);
+		System.out.println(keyword.name);
+		System.out.println(keyword.id);
+	}									
+	
+}
