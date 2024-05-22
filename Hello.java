19c77295636351d86994d32496cc7ca2c6d8942f
MashukQdev <mashukqdev@gmail.com>
2024-05-22 08:13:50 +0530

    Hello.java

--- Hello.java
+++ Hello.java
@@ -5,13 +5,10 @@ Simple program to print hello world
 Practical 1 from core java practicals
 @author Mashuk Patel
  */
-
 public class Hello {
 	
 	public static void main(String[] args) {
-		
 		System.out.println("Hello world");
-		
 	}
 	
 }
