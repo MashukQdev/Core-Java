f0b65fce2da414bd650bddf6063e5126b3f1966b
MashukQdev <mashukqdev@gmail.com>
2024-05-22 16:28:06 +0530

    WrapperClass.java

--- WrapperClass.java
+++ WrapperClass.java
@@ -1,1 +1,21 @@
+package com.main;
 
+/**
+ Simple program to compare variables of wrapper class
+ Practical 9 from core java practicals
+ @author Mashuk Patel
+ */
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		Integer a=new Integer(30);
+		Integer b=new Integer(30);
+		if(a.equals(b)) {
+			System.out.println("Both the variables are same.");
+		}
+		else {
+			System.out.println("Both the variables are not same.");
+		}
+	}
+	
+}
