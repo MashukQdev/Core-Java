5189d3a8ec2dd28832556f33ff364609424ef3aa
MashukQdev <mashukqdev@gmail.com>
2024-05-23 15:29:37 +0530

    ThrowsKeyword.java

--- ThrowsKeyword.java
+++ ThrowsKeyword.java
@@ -1,1 +1,34 @@
+package com.main;
 
+/**
+ Simple program to demonstrate the use of throws keyword
+ Practical 18 from OOPS concept practicals 
+ @author Mashuk Patel
+ */
+public class ThrowsKeyword {
+
+	/**
+	 method to check condition and throw an exception if necessary
+	 * @param number
+	 * @throws NumberFormatException
+	 */
+	public static void calculate(int number) throws NumberFormatException,ArithmeticException {
+		if(number == 0) {
+			throw new NumberFormatException("Number must be greater than 0.");
+		}
+		else {
+			System.out.println("Number is valid.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			calculate(0);
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+		System.out.println("After exception");
+	}
+	
+}
