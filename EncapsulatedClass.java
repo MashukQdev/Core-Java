5c8685c0d1e92c547f12515b8089d6b31cef1f53
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:42:17 +0530

    Update EncapsulatedClass.java

--- EncapsulatedClass.java
+++ EncapsulatedClass.java
@@ -1,4 +1,4 @@
-package com.main;
+package com.oops;
 
 /**
  Simple program to demonstrate use of encapsulation
@@ -10,28 +10,29 @@ class EncapsulatedClass {
 	private String name;
 	
 	/**
-	 getter method
-	 @return string value that is set
+	 Getter method
+	 @return String value that is set
 	 */
 	public String getName() {
 		return name;
 	}
 	
 	/**
-	 setter method
-	 @param name it takes input name
+	 Setter method
+	 @param name It takes input name
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 }
 public class Encapsulation {
-
+	
 	public static void main(String[] args) {
 		EncapsulatedClass demo = new EncapsulatedClass();
-		demo.setName("Mashuk Patel");
-		System.out.println(demo.getName());
+		
+		demo.setName("Mashuk Patel");    // set value to string variable 
+		System.out.println(demo.getName());  // getting value from string variable and printing
 	}
-	
+
 }
