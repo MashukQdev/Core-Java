1c14a929bc5c00b2d04ea1f409dbaa77947e8873
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:48:06 +0530

    Update TryCatchFinally.java

--- TryCatchFinally.java
+++ TryCatchFinally.java
@@ -1,4 +1,4 @@
-package com.main;
+package com.oops;
 
 /**
  Simple program to demonstrate use of try, catch and finally block
@@ -9,18 +9,17 @@ public class TryCatchFinally {
 
 	public static void main(String[] args) {
 		try {
-			System.out.println("Before exception in try block.");
+			System.out.println("Before exception in try block.");    // print statement to indicate its execution befor exception
+			
 			int a = 10 / 0;
 			System.out.println(a);
-			System.out.println("After exception in try block.");
-		}
-		catch(NumberFormatException ex) {
+			
+			System.out.println("After exception in try block.");    // print statement to indicate its execution after exception
+		} catch(NumberFormatException ex) {
 			System.out.println(ex);
-		}
-		finally {
-			System.out.println("This is finally block and it will execute whether exception occured or not.");
+		} finally {
+			System.out.println("This is finally block and it will execute whether exception occured or not.");   // print statement to indicate finally block execution
 		}
 	}
 	
 }
-
