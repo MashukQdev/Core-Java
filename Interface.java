c3a6d618106ce188fb478af82899129d72256429
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:47:14 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,27 +1,36 @@
-package com.main;
+package com.oops;
 
 /**
  Simple program to demonstrate use of interface
  Practical 17 from OOPS concept practicals
  @author Mashuk Patel
  */
+
+/**
+ This interface contain a method to show details of bike bought
+ */
 interface BikeBought {
 	
 	/**
-	 method to display print statement 
-	 * @param personname personname is taken as input
-	 * @param bikename bikename is taken as input
+	 Displaying the details of bike bought
+	 @param personname The name of the person who bought the bike
+	 @param bikename The name of the bike that was bought
 	 */
 	public void showDetails(String personname, String bikename);
 	
 }
 
 /**
- class contain BikeBought interface method that is override which has simple print statement
+ This class implements an interface BikeBought to display details of bike bought 
  */
 class Mashuk implements BikeBought {
 
 	@Override
+	/**
+	 Displaying the details of bike bought
+	 @param personname The name of the person who bought the bike
+	 @param bikename The name of the bike that was bought
+	 */
 	public void showDetails(String personname, String bikename) {
 		System.out.println(personname + " buy a new bike " + bikename);
 	}
@@ -29,11 +38,16 @@ class Mashuk implements BikeBought {
 }
 
 /**
-class contain BikeBought interface method that is override which has simple print statement
+ This class implements an interface BikeBought to display details of bike bought 
 */
 class Anuj implements BikeBought {
 
 	@Override
+	/**
+	 Displaying the details of bike bought
+	 @param personname The name of the person who bought the bike
+	 @param bikename The name of the bike that was bought
+	 */
 	public void showDetails(String personname, String bikename) {
 		System.out.println(personname + " buy a new bike " + bikename);
 	}
@@ -43,9 +57,10 @@ class Anuj implements BikeBought {
 public class Interface {
 
 	public static void main(String[] args) {
-		Mashuk m1 = new Mashuk();
+		Mashuk m1 = new Mashuk();     // create object for class Mashuk
 		m1.showDetails("Mashuk", "KTM");
-		Anuj a1 = new Anuj();
+		
+		Anuj a1 = new Anuj();         // create object for class Anuj
 		a1.showDetails("Anuj", "Bullet");
 	}
 	
