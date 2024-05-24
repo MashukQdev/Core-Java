1eb30e11e09f4b267c09cd40d9a7bb6acc83bdd7
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:57:24 +0530

    Update MapExample.java

--- MapExample.java
+++ MapExample.java
@@ -12,19 +12,22 @@ import java.util.Map;
 public class MapExample {
 
 	public static void main(String[] args) {
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		HashMap<Integer, String> map = new HashMap<Integer, String>();    // initialize map
 		map.put(1, "Hello");
 		map.put(2, "World");
 		map.put(3, "Mashuk");
 		map.put(4, "Ahmedabad");
 		map.put(5, "Himmatnagar");
+		
 		map.put(4, "QDev");  // it will replace corresponding key value
+		
 		map.remove(1);       // delete element using key
+		
 		map.replace(2, "Hello");    // replace corresponding key with new value
+		
 		for(Map.Entry<Integer, String> m1 : map.entrySet()) {
-			System.out.println("Key : " + m1.getKey() + " Value : " + m1.getValue());
+			System.out.println("Key : " + m1.getKey() + " Value : " + m1.getValue());  // printing all map elements
 		}
 	}
 	
 }
-
