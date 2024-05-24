8c9bf4fa7846c3fcc9756626dbc60e08197c05d8
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:56:21 +0530

    Update ListExample.java

--- ListExample.java
+++ ListExample.java
@@ -11,21 +11,27 @@ import java.util.Iterator;
 public class ListExample {
 
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> list = new ArrayList<String>();  // initialize list 
 		list.add("Hello");
 		list.add("World");
 		list.add("Mashuk");
 		list.add("Welcome");
 		list.add("QDev");
 		list.add("Ahmedabad");
+		
 		list.remove("Hello");   // remove using value
+		
 		list.remove(4);         // remove using index
+		
 		list.add(0,"Patel");    // add at start of list 
+		
 		System.out.println("Printing all elements of list using iterator : ");
+		
 		Iterator<String> itr = list.iterator();
 		while(itr.hasNext()) {
 			System.out.println(itr.next());
 		}
+		
 		System.out.println("Get 2 element from list and printing : " + list.get(1)); // get element from list by giving index
 	}
 	
