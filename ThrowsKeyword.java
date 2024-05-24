2df73d90eb7a15ec8b78cbcd3b092a5256f2a289
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:48:55 +0530

    Update ThrowsKeyword.java

--- ThrowsKeyword.java
+++ ThrowsKeyword.java
@@ -1,4 +1,4 @@
-package com.main;
+package com.oops;
 
 /**
  Simple program to demonstrate the use of throws keyword
@@ -8,15 +8,14 @@ package com.main;
 public class ThrowsKeyword {
 
 	/**
-	 method to check condition and throw an exception if necessary
-	 * @param number
-	 * @throws NumberFormatException
+	 This method to check condition and throw an exception if necessary
+	 * @param number The number to be processed
+	 * @throws NumberFormatException If the number is equal to 0
 	 */
 	public static void calculate(int number) throws NumberFormatException,ArithmeticException {
 		if(number == 0) {
 			throw new NumberFormatException("Number must be greater than 0.");
-		}
-		else {
+		} else {
 			System.out.println("Number is valid.");
 		}
 	}
@@ -24,11 +23,11 @@ public class ThrowsKeyword {
 	public static void main(String[] args) {
 		try {
 			calculate(0);
-		}
-		catch(Exception ex) {
+		} catch(Exception ex) {
 			System.out.println(ex);
 		}
-		System.out.println("After exception");
+		
+		System.out.println("After exception");   // printing a statement after exception occurred
 	}
 	
 }
