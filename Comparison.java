c9b5f82ffedf9184492a20e908fc3c19a6f195b4
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:37:32 +0530

    Update Comparison.java

--- Comparison.java
+++ Comparison.java
@@ -10,43 +10,52 @@ import java.math.BigDecimal;
 public class Comparison {
 	
 	/**
-	 method to compare int and Integer with == operator 
+	 Method to compare int and Integer values with == operator 
+	 @return Nothing, print comparison results to the console
 	 */
 	public static void intValueComparison() {
 		//for integer we use == operator to compare primitive data or object
-		int a = 10;
-		int b = 10;
-		Integer c = new Integer(10);
-		boolean result1 = a == b; // comparing two primitive data type (int) will work
+		int firstValue = 10;
+		int secondValue = 10;
+		Integer thirdValue = new Integer(10);
+		
+		boolean result1 = firstValue == secondValue; // comparing two primitive data type (int) will work
 		System.out.println(result1);
-		boolean result2 = b == c; // comparing primitive data type (int) with wrapper class using == operator will work
+		
+		boolean result2 = secondValue == thirdValue; // comparing primitive data type (int) with wrapper class using == operator will work
 		System.out.println(result2);
 	}
 	
 	/**
-	 method to compare string using == operator and equals() method
+	 Method to compare string using == operator and equals() method
+	 @return Nothing, print comparison results to the console
 	 */
 	public static void stringComparison() {
 		//for string
-		String s1="Mashuk";
-		String s2="Mashuk";
-		String s3 = new String("Mashuk");
-		boolean result3 = s1 == s2; // comparing two primitive data type with == operator will work
+		String firstString = "Mashuk";
+		String secondString = "Mashuk";
+		String thirdString = new String("Mashuk");
+		
+		boolean result3 = firstString == secondString; // comparing two primitive data type with == operator will work
 		System.out.println(result3); 
-		boolean result4 = s1 == s3; // comparing primitive data type with wrapper class using == operator will not work
+		
+		boolean result4 = firstString == thirdString; // comparing primitive data type with wrapper class using == operator will not work
 		System.out.println(result4);
-		boolean result5 = s1.equals(s3); // comparing primitive data type with wrapper class using equals() method will work
+		
+		boolean result5 = firstString.equals(thirdString); // comparing primitive data type with wrapper class using equals() method will work
 		System.out.println(result5);
 	}
 	
 	/**
-	 method to compare bigdecimal using equals() method
+	 Method to compare bigdecimal value using equals() method
+	 @return Nothing, print comparison results to the console
 	 */
 	public static void bigDecimalComparison() {
 		//for big decimal we use equals() method for comparison == operator will not work
-		BigDecimal bd1 = new BigDecimal("100.10");
-		BigDecimal bd2 = new BigDecimal("100.10");
-		boolean result6 = bd1.equals(bd2);
+		BigDecimal firstBigDecimalValue = new BigDecimal("100.10");
+		BigDecimal secondBigDecimalValue = new BigDecimal("100.10");
+		
+		boolean result6 = firstBigDecimalValue.equals(secondBigDecimalValue);
 		System.out.println(result6);
 	}
 	
