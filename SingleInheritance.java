90e675b34333f82142fe02f27870b085456a4781
MashukQdev <mashukqdev@gmail.com>
2024-05-24 14:43:15 +0530

    Update SingleInheritance.java

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -1,4 +1,4 @@
-package com.main;
+package com.oops;
 
 /**
  Simple program to demonstrate use of single inheritance
@@ -7,12 +7,12 @@ package com.main;
  */
 
 /**
- simple class which contain print statement and it is parent class
+This class contain method which print a message on console
  */
 class Parent {  // parent class here one parent have only one child
 	
 	/**
-	 simple method to print statement
+	 It print a message to show that it is parent class
 	 */
 	public void print() {
 		System.out.println("This is a parent class.");
@@ -21,12 +21,13 @@ class Parent {  // parent class here one parent have only one child
 }
 
 /**
- simple class which contain print statement and it is child class for parent class
+ This class extend the class Parent 
+ It contain method which print a message on console
  */
 class Child extends Parent {   // child class
 
 	/**
-	 simple method to print statement
+	 It print a message to show that it is child class for class Parent
 	 */
 	public void printStatement() {
 		System.out.println("This is a child class.");
@@ -38,7 +39,7 @@ public class SingleInheritance {
 	
 	public static void main(String[] args) {
 		Child c1 = new Child();
-		c1.print();
+		c1.print();    
 		c1.printStatement();
 	}
 	
