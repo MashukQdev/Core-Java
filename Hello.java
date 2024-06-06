5bfc0ffdbea98e762658a35287746f2ced270918
Mashuk Patel <mashukpatelqdev@gmail.com>
2024-06-06 15:31:46 +0530

    update

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ package com.main;
 /**
 Simple program to print hello world
 Practical 1 from core java practicals
-@author Mashuk Patel
+@author Mashuk Patel from himmatnagar
  */
 public class Hello {
 	
